7b50dcd31dbeb4f1841f475dbf4df60ff69e9787
zanghz <zanghz21@mails.tsinghua.edu.cn>
2024-02-01 22:11:13 +0800

    test message sending

--- account/views.py
+++ account/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello world, Django!")
+    resp= HttpResponse("Hello world, Django!", content_type="text/plain")
+    return resp
