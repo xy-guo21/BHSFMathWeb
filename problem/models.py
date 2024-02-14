40d443b64a323515777e8d941e46bc767b8a93b7
xy-guo21 <xy-guo21@mails.tsinghua.edu.cn>
2024-02-14 11:02:13 +0800

    finish week 3 plan, add gitignore #4

--- problem/models.py
+++ problem/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from taggit.managers import TaggableManager
+from account.models import Student
 
 # Create your models here.
 class Problem(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, blank=True, null=True)
     content = models.TextField(blank=True, null=True)
     answer = models.TextField(blank=True, null=True)
 
@@ -14,6 +15,7 @@ class Problem(models.Model):
     source = models.TextField(blank=True, null=True)
 
     difficulty = models.IntegerField(blank=True, null=True)
+    stars = models.ManyToManyField(to='Student', related_name="starredProblem", blank=True)
 
     image = models.ImageField(upload_to="problemImages", blank=True, null=True)
     answerImage = models.ImageField(upload_to="problemImages", blank=True, null=True)
@@ -38,3 +40,42 @@ class Problem(models.Model):
             ret["imagePath"].append(self.answerImage.url)
 
         return ret
+    
+    def star(self, student: Student):
+        if student in self.stars.all():
+            self.stars.remove(student)
+        else:
+            self.stars.add(student)
+
+
+class Comment(models.Model):
+    problem = models.ForeignKey(to=Problem, on_delete=models.CASCADE)
+    content = models.TextField()
+    image = models.ImageField(upload_to="commentImages", blank=True, null=True)
+    creator = models.ForeignKey(to='Student', on_delete=models.CASCADE)
+    createTime = models.DateTimeField(auto_now_add=True)
+    commentTo = models.ForeignKey(to='self', on_delete=models.CASCADE, blank=True, null=True)
+
+    likes = models.ManyToManyField(to='Student', related_name="likedComment", blank=True)
+    dislikes = models.ManyToManyField(to='Student', related_name="dislikedComment", blank=True)
+
+    def serialize(self):
+        return {
+            "commentID": self.id,
+            "problemID": self.problem.id,
+            "studentID": self.creator.studentID,
+            "commentText": self.content,
+            "commentImage": self.image.url if self.image else None,
+            "createTime": self.createTime,
+            "commentToID": self.commentTo.id if self.commentTo else None,
+            "likes": self.likes.count(),
+            "dislikes": self.dislikes.count(),
+        }
+    
+    def like(self, student: Student):
+        self.likes.add(student)
+        self.dislikes.remove(student)
+
+    def dislike(self, student: Student):
+        self.dislikes.add(student)
+        self.likes.remove(student)
--- problem/views.py
+++ problem/views.py
@@ -6,35 +6,24 @@ from utils.utils_request import BAD_METHOD, request_failed, request_success, ret
 from utils.utils_require import CheckRequire, require
 from utils.utils_time import get_timestamp
 
+from account.models import Student, Admin
+from problem.models import Problem, Comment
 
-from problem.models import Problem
-
-# # create an example
-# if not Problem.objects.filter(id=1):
-#     problem_0 = Problem.objects.create(
-#         title = "解方程：2x + 5 = 11，求 x 的值",
-#         content = "解方程：2x + 5 = 11，求 x 的值",
-#         answer = "x = 3",
-#         difficulty = 2,
-#         source = "数学竞赛 2020",
-#         creator = 1
-#     )
-#     problem_0.topics.add("代数")
 
 # Create your views here.
 def problemQueryID(req: HttpRequest):
      # problem query upon ProblemID
     if req.method == "POST":
-        body = json.loads(req.body.decode("utf-8"))
-        problemID = body.get("problemID")
-
-        # fetch the poblem
-        problem = Problem.objects.filter(id=problemID).first()
-        if not problem:
-            return request_failed("Problem not exists")
+        try:
+            body = json.loads(req.body.decode("utf-8"))
+            problemID = body.get("problemID")
+            problem = Problem.objects.filter(id=problemID).first()
+            assert problem, "Problem not exists"
+            
+            return request_success(problem.serialize())
+        except Exception as e:
+            return request_failed(str(e))
         
-        return request_success(problem.serialize())
-    
     else:
         return BAD_METHOD
     
@@ -42,25 +31,28 @@ def problemQueryID(req: HttpRequest):
 def problemQueryFilter(req: HttpRequest):
      # problem query upon Filter
     if req.method == "POST":
-        body = json.loads(req.body.decode("utf-8"))
-        topic = body.get("topic")
-        difficulty = body.get("difficulty")
-        source = body.get("source")
-        
-        problems = Problem.objects.all()
+        try:
+            body = json.loads(req.body.decode("utf-8"))
+            topic = body.get("topic")
+            difficulty = body.get("difficulty")
+            source = body.get("source")
+            
+            problems = Problem.objects.all()
 
-        if topic: 
-            problems = problems.filter(topics__name__in=[topic])
+            if topic: 
+                problems = problems.filter(topics__name__in=[topic])
 
-        if difficulty:
-            problems = problems.filter(difficulty=int(difficulty))
+            if difficulty:
+                problems = problems.filter(difficulty=int(difficulty))
 
-        if source:
-            problems = problems.filter(source=source)
+            if source:
+                problems = problems.filter(source=source)
 
-        return request_success({
-            "problems": [problem.serialize() for problem in problems]
-        })
+            return request_success({
+                "problems": [problem.serialize() for problem in problems]
+            })
+        except Exception as e:
+            return request_failed(str(e))
     
     else:
         return BAD_METHOD
@@ -68,15 +60,17 @@ def problemQueryFilter(req: HttpRequest):
 def problemQueryDetail(req: HttpRequest):
     # show problem detail
     if req.method == "POST":
-        body = json.loads(req.body.decode("utf-8"))
-        problemID = body.get("problemID")
+        try:
+            body = json.loads(req.body.decode("utf-8"))
+            problemID = body.get("problemID")
 
-        # fetch the poblem
-        problem = Problem.objects.filter(id=problemID).first()
-        if not problem:
-            return request_failed("Problem not exists")
-        
-        return request_success(problem.serialize())
+            # fetch the poblem
+            problem = Problem.objects.filter(id=problemID).first()
+            assert problem, "Problem not exists"
+            
+            return request_success(problem.serialize())
+        except Exception as e:
+            return request_failed(str(e))
     
     else:
         return BAD_METHOD
@@ -84,33 +78,43 @@ def problemQueryDetail(req: HttpRequest):
 def uploadProblem(req: HttpRequest):
     # upload a problem
     if req.method == "POST":
-        body = json.loads(req.body.decode("utf-8"))
-        title = body.get("title")
-        content = body.get("content")
-        answer = body.get("answer")
-        userID = body.get("userID")
-        topics = body.get("topics")
-        source = body.get("source")
-        difficulty = body.get("difficulty")
+        try:
+            studentID = req.COOKIES.get("id")
+            student = Student.objects.filter(studentID=studentID).first()
+            assert student, "Not logged in"
+            
+            body = json.loads(req.body.decode("utf-8"))
+            # title = body.get("title")
+            content = body.get("content")
+            assert content, "Content is required."
 
-        image = req.FILES.get('image')
-        answerImage = req.FILES.get('answerImage')
+            answer = body.get("answer")
+            # userID = body.get("userID")
+            topics = body.get("topics")
+            source = body.get("source")
+            difficulty = body.get("difficulty")
 
-        # create a new problem
-        problem = Problem(
-            title = title,
-            content = content,
-            answer = answer,
-            creator = userID,
-            source = source,
-            difficulty = difficulty,
-            image = image,
-            answerImage = answerImage
-        )
-        problem.save()
-        problem.topics.add(*topics)
+            image = req.FILES.get('image')
+            answerImage = req.FILES.get('answerImage')
 
-        return request_success()
+            # create a new problem
+            problem = Problem(
+                title = content[:20] if len(content) > 20 else content,
+                content = content,
+                answer = answer,
+                creator = studentID,
+                source = source,
+                difficulty = difficulty,
+                image = image,
+                answerImage = answerImage
+            )
+            if topics:
+                problem.topics.add(*topics)
+            problem.save()
+
+            return request_success()
+        except Exception as e:
+            return request_failed(str(e))
     
     else:
         return BAD_METHOD
@@ -119,16 +123,21 @@ def uploadProblem(req: HttpRequest):
 def uploadSolution(req: HttpRequest):
     if req.method == "POST":
         try:
+            studentID = req.COOKIES.get("id")
+            student = Student.objects.filter(studentID=studentID).first()
+            assert student, "Not logged in"
+
             body = json.loads(req.body.decode("utf-8"))
             problemID = body.get("problemID")
+            problem = Problem.objects.filter(id=problemID).first()
+            assert problem, "Problem not exists"
+            assert student == problem.creator, "Not the creator of the problem"
+
             content = body.get("content")
             answerImage = req.FILES.get('answerImage')
             
             assert content or answerImage, "Content or image is required."
 
-            problem = Problem.objects.filter(id=problemID).first()
-            if not problem:
-                return request_failed("Problem not exists")
 
             if content:
                 # if problem.answer:
@@ -149,6 +158,149 @@ def uploadSolution(req: HttpRequest):
                 problem.answerImage = answerImage
 
             problem.save()
+
+            return request_success()
+        
+        except Exception as e:
+            return request_failed(str(e))
+    
+    else:
+        return BAD_METHOD
+
+def querySolution(req: HttpRequest):
+    if req.method == "POST":
+        try:
+            body = json.loads(req.body.decode("utf-8"))
+            problemID = body.get("problemID")
+            problem = Problem.objects.filter(id=problemID).first()
+            assert problem, "Problem not exists"
+
+            return request_success({
+                "problemID": problem.id,
+                "creator": problem.creator,
+                "content": problem.answer,
+                "image": problem.answerImage.url if problem.answerImage else None,
+            })
+        except Exception as e:
+            return request_failed(str(e))
+    
+    else:
+        return BAD_METHOD
+    
+def starProblem(req: HttpRequest):
+    if req.method == "POST":
+        try:
+            studentID = req.COOKIES.get("id")
+            student = Student.objects.filter(studentID=studentID).first()
+            assert student, "Not logged in"
+
+            body = json.loads(req.body.decode("utf-8"))
+            problemID = body.get("problemID")
+            problem = Problem.objects.filter(id=problemID).first()
+            assert problem, "Problem not exists"
+
+            problem.star(student)
+
+            return request_success()
+        
+        except Exception as e:
+            return request_failed(str(e))
+    
+    else:
+        return BAD_METHOD
+
+def uploadComment(req: HttpRequest):
+    if req.method == "POST":
+        try:
+            studentID = req.COOKIES.get("id")
+            student = Student.objects.filter(studentID=studentID).first()
+            assert student, "Not logged in"
+
+            body = json.loads(req.body.decode("utf-8"))
+            problemID = body.get("problemID")
+            problem = Problem.objects.filter(id=problemID).first()
+            assert problem, "Problem not exists"
+
+            commentText = body.get("commentText")
+            commentImage = body.get("commentImage")
+            commentToID = body.get("commentToID")
+            assert commentText or commentImage, "Content or image is required."
+            
+            if commentToID:
+                commentTo = Comment.objects.filter(id=commentToID).first()
+                assert commentTo, "Comment to not exists"
+                
+            # create a new comment
+            comment = Comment(
+                problem = problem,
+                content = commentText,
+                image = commentImage,
+                creator = student,
+                commentTo = commentTo
+            )
+            comment.save()
+            return request_success()
+        
+        except Exception as e:
+            return request_failed(str(e))
+        
+    else:
+        return BAD_METHOD
+    
+def queryComment(req: HttpRequest):
+    if req.method == "POST":
+        try:
+            body = json.loads(req.body.decode("utf-8"))
+            problemID = body.get("problemID")
+            problem = Problem.objects.filter(id=problemID).first()
+            assert problem, "Problem not exists"
+            
+            comments = Comment.objects.filter(problem=problem)
+            return request_success({
+                "comments": [comment.serialize() for comment in comments]
+            })
+        
+        except Exception as e:
+            return request_failed(str(e))
+    
+    else:
+        return BAD_METHOD
+    
+def likeComment(req: HttpRequest):
+    if req.method == "POST":
+        try:
+            studentID = req.COOKIES.get("id")
+            student = Student.objects.filter(studentID=studentID).first()
+            assert student, "Not logged in"
+
+            body = json.loads(req.body.decode("utf-8"))
+            commentID = body.get("commentID")
+            comment = Comment.objects.filter(id=commentID).first()
+            assert comment, "Comment not exists"
+
+            comment.like(student)
+
+            return request_success()
+        
+        except Exception as e:
+            return request_failed(str(e))
+    
+    else:
+        return BAD_METHOD
+
+def dislikeComment(req: HttpRequest):
+    if req.method == "POST":
+        try:
+            studentID = req.COOKIES.get("id")
+            student = Student.objects.filter(studentID=studentID).first()
+            assert student, "Not logged in"
+
+            body = json.loads(req.body.decode("utf-8"))
+            commentID = body.get("commentID")
+            comment = Comment.objects.filter(id=commentID).first()
+            assert comment, "Comment not exists"
+
+            comment.dislike(student)
 
             return request_success()
         
